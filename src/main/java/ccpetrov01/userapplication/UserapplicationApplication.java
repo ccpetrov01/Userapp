3153e955786ebf75c3a14cc1ec71e01a4ac2f95d
Tsvetomir <tanoth2001@abv.bg>
2025-07-02 22:46:17 +0300

    Creating Spring Project

---
+++ src/main/java/ccpetrov01/userapplication/UserapplicationApplication.java
@@ -0,0 +1,13 @@
+package ccpetrov01.userapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ccpetrov01/userapplication/UserapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package ccpetrov01.userapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
