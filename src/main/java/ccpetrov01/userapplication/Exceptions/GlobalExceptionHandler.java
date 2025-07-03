6310e2487afddfcebf514bb38fee0a5b56d94acf
Tsvetomir <tanoth2001@abv.bg>
2025-07-03 20:58:30 +0300

    Updating ExceptionHandler class with more methods.

--- src/main/java/ccpetrov01/userapplication/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/ccpetrov01/userapplication/Exceptions/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package ccpetrov01.userapplication.Exceptions;
 
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
@@ -22,6 +27,25 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body("Validation Failed!" + errors);
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleInvalidJSON(HttpMessageNotReadableException ex){
+        return ResponseEntity.badRequest().body("malformed JSON " + ex.getMostSpecificCause().getMessage());
 
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrity (DataIntegrityViolationException ex){
+        return ResponseEntity.badRequest().body("Database constraint violation:" + ex.getMostSpecificCause().getMessage());
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> handleIllegalException(IllegalStateException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoFound(NoSuchElementException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Resource not found!" + ex.getMessage());
+    }
 }
 
